75ffa9bacf133788560f6f5b8eb49b87868c07a5
Michał Guz <michal.guz.85@gmail.com>
2022-04-21 21:33:43 +0200

    Examples of collectors

---
+++ src/main/java/org/micks/collectors/Joining.java
@@ -0,0 +1,20 @@
+package org.micks.collectors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Joining {
+    public static void run() {
+        List<String> names = new ArrayList<>();
+
+        names.add("Kamil");
+        names.add("Mariusz");
+        names.add("Dominik");
+
+        String joined = names.stream()
+                .collect(Collectors.joining(", ", "Imiona: ", "(wszystkie)"));
+
+        System.out.println(joined);
+    }
+}
---
+++ src/main/java/org/micks/collectors/Main.java
@@ -0,0 +1,13 @@
+package org.micks.collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        ToListSetMap.run();
+//        Joining.run();
+//        CountingSummingAveraging.run();
+//        Summarizing.run();
+        Grouping.run();
+
+    }
+}
